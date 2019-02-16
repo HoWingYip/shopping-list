5220fc5580ca4421d278ce1bb8767bd721a08091
HoWingYip <sploooosh884@gmail.com>
2019-02-16 22:48:29 +0800

    Updates

--- precache-manifest.ad3bececbb0ad09fdf90fe719349fed7.js
+++ precache-manifest.ad3bececbb0ad09fdf90fe719349fed7.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/shopping-list/static/js/runtime~main.8b892420.js"
   },
   {
-    "revision": "351cfaf9d87689ac8995",
-    "url": "/shopping-list/static/js/main.351cfaf9.chunk.js"
+    "revision": "a8c29e2fcac696d52123",
+    "url": "/shopping-list/static/js/main.a8c29e2f.chunk.js"
   },
   {
     "revision": "7e37276a406873370c0f",
     "url": "/shopping-list/static/js/2.7e37276a.chunk.js"
   },
   {
-    "revision": "351cfaf9d87689ac8995",
-    "url": "/shopping-list/static/css/main.b4831f76.chunk.css"
+    "revision": "a8c29e2fcac696d52123",
+    "url": "/shopping-list/static/css/main.48e59131.chunk.css"
   },
   {
-    "revision": "8584fe827cc2f505f6586dfdf3801844",
+    "revision": "ebacbbd95a8f80853d32d03235727fee",
     "url": "/shopping-list/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/shopping-list/precache-manifest.ac23d825048292bfb0b1d3f571f7f70e.js"
+  "/shopping-list/precache-manifest.ad3bececbb0ad09fdf90fe719349fed7.js"
 );
 
 workbox.clientsClaim();
