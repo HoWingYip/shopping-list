6e5fb904860f2c9da1478b7d080a56b1b46b2205
HoWingYip <sploooosh884@gmail.com>
2019-02-16 20:51:24 +0800

    Extracted item component and deleted ItemDisplay component.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import PropTypes from "prop-types";
 import uuidv4 from "uuid/v4";
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = { items: [] };
 
     this.handleKeyPress = this.handleKeyPress.bind(this);
@@ -18,7 +18,7 @@ export default class App extends Component {
     if (event.key === "Enter" && event.target.value.trim() !== "") {
       // basically pushes new item to end of array
       // also trims the string for convenience's sake
-      this.setState({items: [...this.state.items, {
+      this.setState({ items: [...this.state.items, {
         name: event.target.value.trim(),
         id: uuidv4(),
         checked: false,
@@ -64,11 +64,20 @@ export default class App extends Component {
         <input type="text" id="newItemEntry" rows="1" placeholder="Type and press Enter to insert."
         onKeyPress={this.handleKeyPress} />
 
-        <ItemDisplay items={this.state.items}
-          checkItem={this.checkItem}
-          deleteItem={this.deleteItem}
-          updateItem={this.updateItem} />
-
+        <h2>Items</h2>
+        {this.state.items.length === 0 ? (
+          // if list is empty:
+          "Add some items to get started."
+        ) : (
+          // if list is not empty:
+          this.state.items.map((item) =>
+            <Item item={item} key={item.id} itemList={this.state.items}
+              checkItem={this.checkItem}
+              deleteItem={this.deleteItem}
+              updateItem={this.updateItem} />
+          )
+        )}
+    
         {/* only displays clear button if there are items in list */}
         {this.state.items.length > 0 &&
         <div id="clear-items" onClick={this.clearItems}>Clear items</div>}
@@ -77,9 +86,10 @@ export default class App extends Component {
   }
 }
 
-class ItemDisplay extends Component {
+class Item extends Component {
   static propTypes = {
-    items: PropTypes.array.isRequired,
+    item: PropTypes.object.isRequired,
+    itemList: PropTypes.array.isRequired,
     checkItem: PropTypes.func.isRequired,
     deleteItem: PropTypes.func.isRequired,
     updateItem: PropTypes.func.isRequired,
@@ -87,46 +97,32 @@ class ItemDisplay extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        <h2>Items</h2>
+      <div className={this.props.item.checked ? "item-checked" : "item"}>
+        <input type="text" className={this.props.item.checked ? "item-text-checked" : "item-text"}
+          contentEditable={true} defaultValue={this.props.item.name}
+          onChange={(event) => {
+            // submit new item name and item index to function
+            this.props.updateItem(event.target.value, this.props.itemList.indexOf(this.props.item));
+          }} />
 
-        {(this.props.items.length === 0) ? (
-          // if list is empty:
-          "Add some items to get started."
-        ) : (
-          // if list is not empty:
-          this.props.items.map((item) => 
-            <div className={item.checked ? "item-checked" : "item"}
-              key={item.id}>
+        <div className="delete-button item-icon"
+          onClick={() => {
+            // submit item index to function
+            this.props.deleteItem(this.props.itemList.indexOf(this.props.item));
+          }}>
 
-              <input type="text" className={item.checked ? "item-text-checked" : "item-text"}
-                contentEditable={true} defaultValue={item.name}
-                onChange={(event) => {
-                  // submit new item name and item index to function
-                  this.props.updateItem(event.target.value, this.props.items.indexOf(item));
-                }} />
+          <img src={require("./delete.png")} alt="Delete" width="18px" height="18px" />
+        </div>
 
-              <div className="delete-button item-icon"
-                onClick={() => {
-                  // submit item index to function
-                  this.props.deleteItem(this.props.items.indexOf(item));
-                }}>
+        <div className="check-button item-icon"
+          onClick={() => {
+            // submit item index to function
+            this.props.checkItem(this.props.itemList.indexOf(this.props.item));
+          }}>
 
-                <img src={require("./delete.png")} alt="Check" width="18px" height="18px" />
-              </div>
-
-              <div className="check-button item-icon"
-                onClick={() => {
-                  // submit item index to function
-                  this.props.checkItem(this.props.items.indexOf(item));
-                }}>
-
-                <img src={require("./checked.png")} alt="Check" width="18px" height="18px" />
-              </div>
-            </div>
-          )
-        )}
-      </React.Fragment>
+          <img src={require("./checked.png")} alt="Check" width="18px" height="18px" />
+        </div>
+      </div>
     );
   }
 }
