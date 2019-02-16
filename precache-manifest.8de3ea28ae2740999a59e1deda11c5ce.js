718c38074bd8ea26a56dec42fc90406c1011b1ee
HoWingYip <sploooosh884@gmail.com>
2019-02-16 21:02:09 +0800

    Updates

---
+++ precache-manifest.8de3ea28ae2740999a59e1deda11c5ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b892420804a9165fde0",
+    "url": "/shopping-list/static/js/runtime~main.8b892420.js"
+  },
+  {
+    "revision": "5eefd5ffb319af126efa",
+    "url": "/shopping-list/static/js/main.5eefd5ff.chunk.js"
+  },
+  {
+    "revision": "7e37276a406873370c0f",
+    "url": "/shopping-list/static/js/2.7e37276a.chunk.js"
+  },
+  {
+    "revision": "5eefd5ffb319af126efa",
+    "url": "/shopping-list/static/css/main.164eacd7.chunk.css"
+  },
+  {
+    "revision": "621e33060651a5f5c47f7929e9bd9a13",
+    "url": "/shopping-list/index.html"
+  }
+];
